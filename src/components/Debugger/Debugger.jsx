9c03798651f0128646946139d2fa50f7eda66d25
Trezy <t@trezy.com>
2023-09-17 08:36:58 -0500

    hide debugger by default

--- src/components/Debugger/Debugger.jsx
+++ src/components/Debugger/Debugger.jsx
@@ -1,4 +1,8 @@
 // Module imports
+import {
+	useCallback,
+	useState,
+} from 'react'
 import { store } from '../../store/store.js'
 import { useStore } from 'statery'
 
@@ -19,10 +23,49 @@ import styles from './Debugger.module.scss'
  * @component
  */
 export function Debugger() {
+	const [shouldShowDebugger, setShouldShowDebugger] = useState(false)
+
 	const {
 		characterQueue,
 		characterQueueIndex,
 	} = useStore(store)
+
+	const hideDebugger = useCallback(() => {
+		setShouldShowDebugger(false)
+	}, [setShouldShowDebugger])
+
+	const showDebugger = useCallback(() => {
+		setShouldShowDebugger(true)
+	}, [setShouldShowDebugger])
+
+	const toggleDebugger = useCallback(() => {
+		if (shouldShowDebugger) {
+			hideDebugger()
+		} else {
+			showDebugger()
+		}
+	}, [
+		hideDebugger,
+		shouldShowDebugger,
+		showDebugger,
+	])
+
+	if (typeof window !== 'undefined') {
+		/* eslint-disable @typescript-eslint/ban-ts-comment */
+
+		// @ts-ignore
+		window.hideDebugger = hideDebugger
+		// @ts-ignore
+		window.showDebugger = showDebugger
+		// @ts-ignore
+		window.toggleDebugger = toggleDebugger
+
+		/* eslint-enable @typescript-eslint/ban-ts-comment */
+	}
+
+	if (!shouldShowDebugger) {
+		return null
+	}
 
 	return (
 		<div className={styles['debugger']}>
