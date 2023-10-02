93005c96abfc276f299f142dab35ddcdd01035bd
Trezy <t@trezy.com>
2023-10-02 15:52:50 -0500

    make the loading screen not look like garbage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,30 +1,8 @@
-// Module imports
-import dynamic from 'next/dynamic'
+// Local imports
+import { HomePage } from '../components/HomePage/HomePage.jsx'
 
 
 
 
 
-// Constants
-const App = dynamic(() => {
-	return import('../components/AppWrapper/AppWrapper.jsx')
-		.then(mod => mod.AppWrapper)
-}, {
-  // eslint-disable-next-line jsdoc/require-jsdoc
-  loading: () => <div>{'Loading...'}</div>,
-})
-
-
-
-
-
-/**
- * The home page.
- *
- * @component
- */
-export default function HomePage() {
-	return (
-		<App />
-	)
-}
+export default HomePage
