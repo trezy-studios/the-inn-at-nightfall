f0bb539e35bcf719bcc1f1b531e07e00079f25b0
Trezy <t@trezy.com>
2023-10-20 15:50:51 -0500

    fill screen on launch

--- src/main/helpers/createWindow.js
+++ src/main/helpers/createWindow.js
@@ -1,5 +1,8 @@
 // Module imports
-import { BrowserWindow } from 'electron'
+import {
+	BrowserWindow,
+	screen,
+} from 'electron'
 import path from 'path'
 
 
@@ -10,12 +13,24 @@ import path from 'path'
  * Creates a new window.
  */
 export function createWindow() {
+	// Get the resolution of the current screen.
+	const display = screen.getPrimaryDisplay()
+
 	const mainWindow = new BrowserWindow({
-		width: 800,
-		height: 600,
+		autoHideMenuBar: true,
+		backgroundColor: '#000000',
+		height: display.workArea.height,
+		show: false,
+		title: 'The Inn at Nightfall',
 		webPreferences: {
 			preload: path.join(__dirname, 'preload.js'),
+			backgroundThrottling: false,
+			contextIsolation: false,
+			nodeIntegration: false,
 		},
+		width: display.workArea.width,
+		x: display.workArea.x,
+		y: display.workArea.y,
 	})
 
 	if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
@@ -24,5 +39,5 @@ export function createWindow() {
 		mainWindow.loadFile(path.join(__dirname, '..', '..', 'renderer', MAIN_WINDOW_VITE_NAME, 'index.html'))
 	}
 
-	mainWindow.webContents.openDevTools()
+	mainWindow.once('ready-to-show', () => mainWindow.show())
 }
