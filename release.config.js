31d7b00a54db29a7b6f0e132e6beccb81b728436
Trezy <t@trezy.com>
2023-10-24 11:29:04 -0500

    feat: add semantic-release

---
+++ release.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+	branches: [
+		'main',
+		{
+			name: 'beta',
+			prerelease: true,
+		},
+	],
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		'@semantic-release/changelog',
+		'semantic-release-export-data',
+		['@semantic-release/npm', {
+			npmPublish: false,
+		}],
+		['@semantic-release/git', {
+			assets: [
+				'CHANGELOG.md',
+				'package.json',
+				'yarn.lock',
+			],
+		}],
+	],
+}
