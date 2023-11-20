d465833836712eb3d71a68f4f6f6c33ef21cba94
Trezy <t@trezy.com>
2023-11-20 11:30:58 -0600

    ci(semantic-release): remove unnecessary plugins
    I'm pretty confident we don't actually need either of these plugins.

--- release.config.js
+++ release.config.js
@@ -10,15 +10,5 @@ module.exports = {
 		'@semantic-release/commit-analyzer',
 		'@semantic-release/release-notes-generator',
 		'semantic-release-export-data',
-		['@semantic-release/npm', {
-			npmPublish: false,
-		}],
-		['@semantic-release/git', {
-			assets: [
-				'CHANGELOG.md',
-				'package.json',
-				'yarn.lock',
-			],
-		}],
 	],
 }
