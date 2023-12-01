cca965ca42d13ba15b7cc12675cb73a946c3d834
Trezy <t@trezy.com>
2023-12-01 16:02:52 -0600

    feat(ui): add the new chrome

--- src/renderer/components/GameDialog/GameDialog.module.scss.d.ts
+++ src/renderer/components/GameDialog/GameDialog.module.scss.d.ts
@@ -1,7 +1,7 @@
 declare const classNames: {
+  readonly "game-dialog-wrapper": "game-dialog-wrapper";
   readonly "game-dialog": "game-dialog";
   readonly messages: "messages";
   readonly author: "author";
-  readonly responses: "responses";
 };
 export = classNames;
--- src/renderer/components/Modal/Modal.module.scss.d.ts
+++ src/renderer/components/Modal/Modal.module.scss.d.ts
@@ -2,5 +2,8 @@ declare const classNames: {
   readonly "modal-backdrop": "modal-backdrop";
   readonly "modal-wrapper": "modal-wrapper";
   readonly modal: "modal";
+  readonly "bottom-frame": "bottom-frame";
+  readonly "top-frame": "top-frame";
+  readonly "center-decoration": "center-decoration";
 };
 export = classNames;
