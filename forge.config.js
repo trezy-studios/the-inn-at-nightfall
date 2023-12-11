a45a358e2a8bb40629e7bab694c3982350b7d90d
Trezy <t@trezy.com>
2023-12-11 10:25:24 -0600

    ci(github actions): remove unused Electron tools (and Flatpak)

--- forge.config.js
+++ forge.config.js
@@ -1,10 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 // Module imports
-const { MakerDeb } = require('@electron-forge/maker-deb')
-const { MakerDMG } = require('@electron-forge/maker-dmg')
-const { MakerRpm } = require('@electron-forge/maker-rpm')
-const { MakerSquirrel } = require('@electron-forge/maker-squirrel')
 const { PublisherGithub } = require('@electron-forge/publisher-github')
 const { VitePlugin } = require('@electron-forge/plugin-vite')
 
@@ -19,31 +15,14 @@ const packageData = require('./package.json')
 
 
 
-const makers = []
-
-switch (process.platform) {
-	case 'darwin':
-		makers.push(new MakerDMG({}))
-		break
-
-	case 'linux':
-		makers.push(new MakerDeb({}))
-		makers.push(new MakerRpm({}))
-		break
-
-	case 'win32':
-		makers.push(new MakerSquirrel({}))
-		break
-
-	default:
-}
-
 module.exports = {
 	packagerConfig: {
 		executableName: packageData.name,
 	},
 	rebuildConfig: {},
-	makers,
+	makers: [
+		{ name: '@electron-forge/maker-zip' },
+	],
 	plugins: [
 		new VitePlugin({
 			build: [
